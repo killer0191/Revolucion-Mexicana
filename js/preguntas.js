f9444ec0a6a2c15e0555700f1e90516d7d1eecbb
killer0191 <99231496+killer0191@users.noreply.github.com>
2023-06-06 22:34:03 -0600

    Add files via upload

---
+++ js/preguntas.js
@@ -0,0 +1,125 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "¿En qué año inició la Revolución Mexicana?",
+    answer: "1910",
+    options: ["1960", "1950", "1910", "1900"],
+  },
+  {
+    numb: 2,
+    question:
+      "¿Quién fue el líder del Ejército Zapatista durante la Revolución Mexicana?",
+    answer: "Emiliano Zapata",
+    options: [
+      "Emiliano Zapata",
+      "Francisco Madero",
+      "Venustiano Carranza",
+      "Miguel Hidalgo",
+    ],
+  },
+  {
+    numb: 3,
+    question: "¿Cuál fue la causa principal de la Revolución Mexicana?",
+    answer: "La desigualdad social y la pobreza",
+    options: [
+      "La influencia extranjera en la política mexicana",
+      "La desigualdad social y la pobreza",
+      "Los pasteles que se perdieron",
+      "La lucha por la independencia de México",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "¿Quién fue el presidente de México antes de la Revolución Mexicana?",
+    answer: "Porfirio Díaz",
+    options: [
+      "Maximiliano de Habsburgo",
+      "Antonio Lopéz de Santana",
+      "Benito Juárez",
+      "Porfirio Díaz",
+    ],
+  },
+  {
+    numb: 5,
+    question:
+      "¿Qué documento marcó el inicio oficial de la Revolución Mexicana?",
+    answer: "El Plan de San Luis Potosí",
+    options: [
+      "La Constitución de 1857",
+      "La perdida del territorio nacional",
+      "El Plan de San Luis Potosí",
+      "El Plan de Ayala",
+    ],
+  },
+  {
+    numb: 6,
+    question:
+      "¿En qué año se promulgó la Constitución Política de los Estados Unidos Mexicanos?",
+    answer: "1917",
+    options: ["1910", "1917", "1937", "1950"],
+  },
+  {
+    numb: 7,
+    question:
+      "¿Qué papel desempeñó Francisco I. Madero en la Revolución Mexicana?",
+    answer: "Fue el presidente de México antes de la Revolución",
+    options: [
+      "Fue el líder del Ejército Constitucionalista",
+      "Fue el líder del Plan de San Luis",
+      "Fue el líder del Plan de Ayala",
+      "Fue el presidente de México antes de la Revolución",
+    ],
+  },
+  {
+    numb: 8,
+    question:
+      "¿Qué presidente mexicano fue derrocado por la Revolución Mexicana?",
+    answer: "Porfirio Díaz",
+    options: [
+      "Porfirio Díaz",
+      "Lázaro Cárdenas",
+      "Vicente Fox",
+      "Benito Juárez",
+    ],
+  },
+  {
+    numb: 9,
+    question:
+      "¿Cuál fue el famoso lema de Emiliano Zapata durante la Revolución Mexicana?",
+    answer: "¡Tierra y Libertad!",
+    options: [
+      "¡Libertad o muerte!",
+      "¡Viva la Revolución!",
+      "¡Tierra y Libertad!",
+      "¡Viva México!",
+    ],
+  },
+  {
+    numb: 10,
+    question:
+      "¿Quién fue el líder del Ejército Constitucionalista durante la Revolución Mexicana?",
+    answer: "El Plan de San Luis Potosí",
+    options: [
+      "Francisco Villa",
+      "Emiliano Zapata",
+      "Pancho Villa",
+      "Venustiano Carranza",
+    ],
+  },
+  // puede descomentar los códigos a continuación y duplicarlos tanto como desee agregar una pregunta
+  // pero recuerde que debe dar el valor adormecido serializar como 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Tu pregunta está aquí",
+  //   answer: "La respuesta correcta de la pregunta está aquí.",
+  //   options: [
+  //     "Opción 1",
+  //     "Opción 2",
+  //     "Opción 3",
+  //     "Opción 4"
+  //   ]
+  // },
+];
